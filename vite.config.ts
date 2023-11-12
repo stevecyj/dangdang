f0979fad0daa22435f2cafc6bd3fd1a5b60a1965
stevecyj <ycsteven.tsao@gmail.com>
2023-11-12 21:13:10 +0800

    chore: initialize server configuration in `vite.config.ts`
    - Add `CommonServerOptions` import from `vite` in `vite.config.ts`
    - Add `server` variable initialization
    - Set `port` and `proxy` properties in `server` object
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, CommonServerOptions } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -7,13 +7,25 @@ export default defineConfig((mode) => {
   const envFileName: string = ".env";
   const curEnvFileName: string = `${envFileName}.${mode.mode}`;
   console.log("curEnvFileName", curEnvFileName);
+
+  let server: CommonServerOptions = {};
   if (mode.mode === "dev") {
-    console.log("dev mode");
+    server = {
+      // host: "192.168.2.6",
+      port: 5005,
+      proxy: {
+        '/dang': {
+          target: "http://192.168.2.6:5003",
+        },
+      },
+    };
+    console.log("dev mode", server);
   } else if (mode.mode === "prod") {
     console.log("prod mode");
   }
 
   return {
     plugins: [vue()],
+    server,
   };
 });
