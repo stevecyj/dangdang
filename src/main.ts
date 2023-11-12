0a68ec9c77b514e76d011946fcc6dfd776a9a66f
stevecyj <ycsteven.tsao@gmail.com>
2023-11-12 17:01:06 +0800

    chore: update environment variable handling
    - Add `.env` file with environment variables
    - Add `.env.dev` file with environment variables
    - Update `dev` script in `package.json` to include `--mode dev`
    - Add console logs to `main.ts` to display environment variables
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+console.log('env', import.meta.env)
+console.log('env app title', import.meta.env.VITE_APP_TITLE)
+console.log('env user', import.meta.env.VITE_USER);
+
 createApp(App).mount('#app')
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_USER: string;
+  VITE_APP_TITLE: string;
+}
