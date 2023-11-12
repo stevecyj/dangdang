cc90c5706adaacccd0a9767800f29c1169da4d2b
stevecyj <ycsteven.tsao@gmail.com>
2023-11-12 17:22:40 +0800

    chore: update build script and configuration for production mode
    - Update the build script in package.json to include the `--mode prod` flag
    - Comment out console log statements in main.ts
    - Add environment-specific logic to vite.config.ts
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+
+export default defineConfig((mode) => {
+  const envFileName: string = ".env";
+  const curEnvFileName: string = `${envFileName}.${mode.mode}`;
+  console.log("curEnvFileName", curEnvFileName);
+  if (mode.mode === "dev") {
+    console.log("dev mode");
+  } else if (mode.mode === "prod") {
+    console.log("prod mode");
+  }
+
+  return {
+    plugins: [vue()],
+  };
+});
