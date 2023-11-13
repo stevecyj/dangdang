3b835b7334945735b6cba7da75e05901fedbefb8
stevecyj <ycsteven.tsao@gmail.com>
2023-11-14 07:26:58 +0800

    chore: update eslint configuration for Vue 3 support
    - Add new file `.eslintrc.cjs` with eslint configuration
    - Set `root` property to `true` in eslint configuration
    - Add global variables `defineProps`, `defineEmits`, `defineExpose`, `withDefaults` in eslint configuration
    - Set `browser` and `es2021` to `true` in eslint environment configuration
    - Extend eslint configuration with `standard-with-typescript` and `plugin:vue/vue3-essential`
    - Add `overrides` section in eslint configuration
    - Set `node` to `true` in `env` property of `overrides`
    - Add `files` property in `overrides` section of eslint configuration
    - Set `sourceType` to `script` in `parserOptions` of `overrides` section
    - Set `ecmaVersion` to `latest` and `sourceType` to `module` in `parserOptions` of eslint configuration
    - Add `vue` plugin in eslint configuration
    - Set `space-before-function-paren` rule to `0` in eslint configuration
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+    "root": true,
+    "globals": {
+        "defineProps": "readonly",
+        "defineEmits": "readonly",
+        "defineExpose": "readonly",
+        "withDefaults": "readonly"
+    },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "space-before-function-paren": 0,
+    }
+}
