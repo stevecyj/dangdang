e43e614c60a2fbe60198850d6649679d030ad02f
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-13 14:37:06 +0800

    feat: refactor image loading process
    - Add `ImgUtil` class to handle image loading
    - Call `ImgUtil.loadAllImg()` to load all images
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/utils/imgUtil.ts
@@ -0,0 +1,20 @@
+export class ImgUtil {
+  static imgList: Record<string, string> = {};
+  static loadAllImg() {
+    const imgMap = import.meta.glob("../assets/img/**/*.{jpg,png}", {
+      eager: true,
+      import: "default",
+    });
+    console.log("imgMap", imgMap);
+    let absolutePath: string = ""; //絕對路徑
+    let imgName: string = ""; //檔名
+    for (let relativePath in imgMap) {
+      // console.log(imgMap[relativePath]);
+      absolutePath = imgMap[relativePath] as string;
+      imgName = absolutePath.substring(absolutePath.lastIndexOf("/") + 1);
+      // console.log("imgName", imgName);
+      this.imgList[imgName] = absolutePath;
+    }
+    console.log("this.imgList", this.imgList);
+  }
+}
