cd6a3341ab59816415663907aeaece55c3665d76
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-14 15:58:19 +0800

    style: use single quotes for string literals and update proxy target URL
    - Change the import syntax from double quotes to single quotes
    - Change the console.log statements to use single quotes instead of double quotes
    - Remove unnecessary console.log statements
    - Update the proxy target URL to use single quotes instead of double quotes
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,41 @@
-import { defineConfig, CommonServerOptions } from "vite";
-import vue from "@vitejs/plugin-vue";
-import fs from "fs";
-import dotenv, { DotenvParseOutput } from "dotenv";
+import { defineConfig, type CommonServerOptions } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import dotenv, { type DotenvParseOutput } from 'dotenv';
 
 // https://vitejs.dev/config/
 
 export default defineConfig((mode) => {
-  const envFileName: string = ".env";
+  const envFileName: string = '.env';
   const curEnvFileName: string = `${envFileName}.${mode.mode}`;
-  console.log("curEnvFileName", curEnvFileName);
+  console.log('curEnvFileName', curEnvFileName);
 
   let server: CommonServerOptions = {};
   const envData = fs.readFileSync(curEnvFileName);
   const envMap: DotenvParseOutput = dotenv.parse(envData);
-  console.log("envMap", envMap);
+  console.log('envMap', envMap);
 
-  if (mode.mode === "dev") {
+  if (mode.mode === 'dev') {
     server = {
       host: envMap.VITE_HOST,
       port: envMap.VITE_PORT,
       proxy: {
-        "/dang": {
-          target: "http://192.168.2.6:5003",
-        },
-      },
+        '/dang': {
+          target: 'http://192.168.2.6:5003'
+        }
+      }
     };
-    console.log("dev mode", server);
-  } else if (mode.mode === "prod") {
+    console.log('dev mode', server);
+  } else if (mode.mode === 'prod') {
     server = {
       host: envMap.VITE_HOST,
-      port: envMap.VITE_PORT,
+      port: envMap.VITE_PORT
     };
-    console.log("prod mode");
+    console.log('prod mode');
   }
 
   return {
     plugins: [vue()],
-    server,
+    server
   };
 });
