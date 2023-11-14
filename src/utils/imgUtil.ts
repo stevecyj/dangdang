d4bbdd31576ba4ae1eabcdff34c49b4ac43171a4
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-14 12:02:23 +0800

    style: improve code style and type annotations
    - Change the import statement for `good-storage` from double quotes to single quotes
    - Update the type annotation for `imgListFromStorage` to be more specific
    - Add type annotation for the return type of `isEmpty` method
    - Remove unnecessary semicolons at the end of lines
    - Fix the import statement for `import.meta.glob` to use single quotes instead of double quotes
    - Remove unnecessary console.log statements
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/utils/imgUtil.ts
+++ src/utils/imgUtil.ts
@@ -1,40 +1,49 @@
-import goodStorage from "good-storage";
+import goodStorage from 'good-storage'
 export class ImgUtil {
-  static imgList: Record<string, string> = {};
+  static imgList: Record<string, string> = {}
 
-  static storageImgList() {
-    this.imgList = goodStorage.get("imgList") || {};
-    if(this.isEmpty()){
-      this.loadAllImg();
-      goodStorage.set("imgList", this.imgList);
+  static storageImgList (): void {
+    // Check if the result from goodStorage.get('imgList') is an object
+    const imgListFromStorage = goodStorage.get('imgList')
+    if (typeof imgListFromStorage === 'object' && imgListFromStorage !== null) {
+      // It's an object, so cast it to the expected type
+      this.imgList = imgListFromStorage as Record<string, string>
+    } else {
+      // If it's not an object, initialize it as an empty object
+      this.imgList = {}
+    }
+
+    if (this.isEmpty()) {
+      this.loadAllImg()
+      goodStorage.set('imgList', this.imgList)
     }
   }
 
-  static isEmpty() {
-    return !Object.getOwnPropertyNames(this.imgList).length;
+  static isEmpty (): boolean {
+    return Object.getOwnPropertyNames(this.imgList).length === 0
   }
 
-  static getImg(imgName: string): string {
-    return ImgUtil.imgList[imgName];
+  static getImg (imgName: string): string {
+    return ImgUtil.imgList[imgName]
   }
 
-  static loadAllImg() {
-    const imgMap = import.meta.glob("../assets/img/**/*.{jpg,png}", {
+  static loadAllImg (): void {
+    const imgMap = import.meta.glob('../assets/img/**/*.{jpg,png}', {
       eager: true,
-      import: "default",
-    });
-    console.log("imgMap", imgMap);
-    let absolutePath: string = ""; //絕對路徑
-    let imgName: string = ""; //檔名
-    for (let relativePath in imgMap) {
+      import: 'default'
+    })
+    console.log('imgMap', imgMap)
+    let absolutePath: string = '' // 絕對路徑
+    let imgName: string = '' // 檔名
+    for (const relativePath in imgMap) {
       // console.log(imgMap[relativePath]);
-      absolutePath = imgMap[relativePath] as string;
-      imgName = absolutePath.substring(absolutePath.lastIndexOf("/") + 1);
+      absolutePath = imgMap[relativePath] as string
+      imgName = absolutePath.substring(absolutePath.lastIndexOf('/') + 1)
       // console.log("imgName", imgName);
-      this.imgList[imgName] = absolutePath;
+      this.imgList[imgName] = absolutePath
     }
-    console.log("this.imgList", this.imgList);
+    console.log('this.imgList', this.imgList)
   }
 }
 
-export default ImgUtil.getImg;
+export default ImgUtil.getImg
