b23077806c59d7b67cd276e453de0a936e09c62b
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-14 12:45:14 +0800

    chore: update eslint and typescript configurations
    - Update `.eslintrc.cjs` file
    - Add new global variables to `.eslintrc.cjs`
    - Update environment settings in `.eslintrc.cjs`
    - Update extended configurations in `.eslintrc.cjs`
    - Update override configurations in `.eslintrc.cjs`
    - Update parser and parserOptions in `.eslintrc.cjs`
    - Update plugins and rules in `.eslintrc.cjs`
    - Update `tsconfig.json` file
    - Add `.eslintrc.cjs` to the file list in `tsconfig.json`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,52 @@
 module.exports = {
-    "root": true,
-    "globals": {
-        "defineProps": "readonly",
-        "defineEmits": "readonly",
-        "defineExpose": "readonly",
-        "withDefaults": "readonly"
-    },
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "parser": "@typescript-eslint/parser",
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": ['tsconfig.json'],
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-        "space-before-function-paren": 0,
+  root: true,
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly'
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'space-before-function-paren': 0,
+    'vue/no-multiple-template-root': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    semi: 0,
+    quotes: [2, 'single'],
+    'prefer-const': 2,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-inferrable-types': 0
+  }
 }
